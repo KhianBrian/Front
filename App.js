d16c4743308cbf61990e0f84155402a564af259e
Keyannish <briansismundo@gmail.com>
2025-06-04 10:40:42 +0800

    added new file

--- App.js
+++ App.js
@@ -2,16 +2,42 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import StartScreen from './screens/StartScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import MainTabs from './navigation/MainTabs';
+import { ExpenseProvider } from './context/ExpenseContext';
+import { UserProvider } from './context/UserContext'; 
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Start">
-        <Stack.Screen name="Start" component={StartScreen} options={{ headerShown: false }} />
-        {/* Add your Home screen later here */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <UserProvider> 
+      <ExpenseProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Start" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Start" component={StartScreen}  options={{
+                title: '',
+                headerShown: true,
+                headerStyle: {
+                  backgroundColor: '#FFC107',
+                },
+                headerTitleAlign: 'center',
+                headerTintColor: '#000', 
+              }}/>
+            <Stack.Screen name="Register" component={RegisterScreen}  options={{
+                title: 'Register',
+                headerShown: true,
+                headerStyle: {
+                  backgroundColor: '#FFC107',
+                },
+                headerTitleAlign: 'center',
+                headerTintColor: '#000',
+              }}/>
+            <Stack.Screen name="MainTabs" component={MainTabs} options={{ headerShown: false }}
+/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ExpenseProvider>
+    </UserProvider>
   );
 }
